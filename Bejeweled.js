e6eb07b6063a679376a174935765318471982eeb
laurapieters <laurapieters@home.nl>
2020-12-10 11:04:07 +0100

    added removing rows and filling

--- Bejeweled.js
+++ Bejeweled.js
@@ -125,38 +125,101 @@ class Grid{
             }
         }
         if (this.clickedJewels.length === 2) {
-            console.log(this.clickedJewels);
-            // saving
-            const firstJewel = this.clickedJewels[0];
-            const secondJewel = this.clickedJewels[1];
-            const firstX = firstJewel.x;
-            const firstY = firstJewel.y;
-            const secondX = secondJewel.x;
-            const secondY = secondJewel.y;
-
             // switching
-            this.jewels[firstJewel.x][firstJewel.y] = secondJewel;
-            firstJewel.x = secondX;
-            firstJewel.y = secondY;
-            this.jewels[secondJewel.x][secondJewel.y] = firstJewel;
-            secondJewel.x = firstX;
-            secondJewel.y = firstY;
+            const firstColor = this.clickedJewels[0].color;
+            const secondColor = this.clickedJewels[1].color;
+            this.jewels[this.clickedJewels[0].x][this.clickedJewels[0].y].color = secondColor;
+            this.jewels[this.clickedJewels[1].x][this.clickedJewels[1].y].color = firstColor;
 
             // displaying
-            const element = document.getElementById('grid');
-            element.parentNode.removeChild(element);
-            const gridDiv = this.createGrid();
-            document.body.appendChild(gridDiv);
+            this.displayAgain();
             this.clickedJewels = [];
 
-            // check for row, remove and display again
-            // ...
+
+            const removeAndFill = () => {
+                this.foundRows = this.checkForRow();
+                if(this.foundRows.length === 0){
+                    return;
+                }
+                // check for row, remove and display again
+                setTimeout(() => {
+                    // this.foundRows = this.checkForRow();
+                    this.removeRows(this.foundRows);
+                    this.displayAgain();
+
+                    // move jewels down
+                    setTimeout(() => {
+                        this.fallDown();
+                        this.displayAgain();
+
+                        // generate new jewels
+                        setTimeout(() => {
+                            this.generateNewJewels();
+                            this.displayAgain();
+                            removeAndFill();
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }
+            removeAndFill();
+
+        }
+    }
+
+    displayAgain(){
+        const element = document.getElementById('grid');
+        element.parentNode.removeChild(element);
+        const gridDiv = this.createGrid();
+        document.body.appendChild(gridDiv);
+    }
+
+    removeRows(foundRows){
+        for(let i = 0; i < foundRows.length; i++){
+            this.jewels[foundRows[i][0].x][foundRows[i][0].y] = new Jewel(foundRows[i][0].x, foundRows[i][0].y, 'empty',this.switchCallback);
+            this.jewels[foundRows[i][1].x][foundRows[i][1].y] = new Jewel(foundRows[i][1].x, foundRows[i][1].y, 'empty',this.switchCallback);
+            this.jewels[foundRows[i][2].x][foundRows[i][2].y] = new Jewel(foundRows[i][2].x, foundRows[i][2].y, 'empty',this.switchCallback);
         }
     }
 
     fallDown(){
-
+        let firstEmpty = 100;
+        let lastEmpty = 100;
+        for(let i = 0; i < 8; i++){
+            for(let j = 0; j < 8; j++){
+                if((j !== 0 && this.jewels[i][j].color === 'empty' && this.jewels[i][j-1].color !== 'empty') ||
+                    (j === 0 && this.jewels[i][j].color === 'empty')){
+                    firstEmpty = j;
+                }
+                if((j !== 7 && this.jewels[i][j].color === 'empty' && this.jewels[i][j+1].color !== 'empty') ||
+                    (j === 7 && this.jewels[i][j].color === 'empty')){
+                    lastEmpty = j;
+                }
+            }
+            if(firstEmpty !== 0 && firstEmpty !== 100){
+                for(let j = 0; j < firstEmpty; j++){
+                    this.jewels[i][lastEmpty-j].color = this.jewels[i][firstEmpty-j-1].color;
+                    this.jewels[i][firstEmpty-j-1].color = 'empty';
+                }
+            }
+            firstEmpty = 100;
+            lastEmpty = 100;
+        }
     }
+
+    generateNewJewels(){
+        const colors = ['darkblue', 'darkpink', 'green', 'lightblue', 'lightpink', 'orange',
+            'purple', 'red', 'turquois'];
+        for(let j = 0; j < 8; j++){
+            for(let i = 0; i < 8; i++) {
+                if(this.jewels[i][j].color === 'empty'){
+                    const nr = Math.floor(Math.random() * (colors.length));
+                    const color =  colors[nr];
+                    this.jewels[i][j].color = color;
+                }
+            }
+        }
+    }
+
 }
 
 class Jewel{
